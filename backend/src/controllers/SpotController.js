793b4d5f25690c045e54c1306d970383abf5470e
Raphael Fontoura <raphael.fontoura@gmail.com>
2019-10-03 12:42:02 -0300

    Criado o dashboard e o booking e ajustado o upload de arquivos

--- backend/src/controllers/SpotController.js
+++ backend/src/controllers/SpotController.js
@@ -1,8 +1,35 @@
-module.exports = {
-    async store(req,res) {
-        console.log(req.body);
-        console.log(req.file);
+const Spot = require('../models/Spot');
+const User = require('../models/User');
 
-        return res.json({ ok: true });
+module.exports = {
+    async index(req, res) {
+        const { tech } = req.query;
+
+        const spots = await Spot.find({ techs: tech });
+
+        return res.json(spots);
+    },
+
+    async store(req,res) {
+        const { filename } = req.file;
+        const { company, techs, price } = req.body;
+        const { user_id } = req.headers;
+
+        //validando o usuario, se existe e, caso não exista, retornar um status de erro.
+        const user = await User.findById(user_id);
+
+        if (!user) {
+            return res.status(400).json({ error: 'User does not exists' })
+        }
+
+        const spot = await Spot.create({
+            user: user_id,
+            thumbnail: filename,
+            company,
+            techs: techs.split(',').map(tech => tech.trim()),
+            price
+        });
+
+        return res.json(spot);
     }
 };
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
-const SessionController = require('./controllers/SessionController')
-const SpotController = require('./controllers/SpotController')
+const SessionController = require('./controllers/SessionController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 
 const routes = express.Router();
@@ -11,8 +13,13 @@ const upload = multer(uploadConfig);
 
 
 routes.post('/sessions', SessionController.store);
+
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
 
+routes.get('/dashboard', DashboardController.show);
+
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 //routes.post('/users', (req, res) => {
 //    return res.json(req.body);
