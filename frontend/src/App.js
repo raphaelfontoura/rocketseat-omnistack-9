c4e80a571ef695d02580c55713ec36174f8d0d45
Raphael Fontoura <raphael.fontoura@gmail.com>
2019-10-04 17:21:06 -0300

    Adicionado o componente: npm add react-router-dom ; criado as pastas login, dashboard e new com arquivos index.js e levado a parte responsavel do login de dentro do App.js para Login/index.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,20 @@
-import React, { useState } from 'react';
-import api from './services/api';
+import React from 'react';
+
 import './App.css';
 
 import logo from './assets/logo.svg';
 
+import Routes from './routes';
+
 function App() {
-  const [email, setEmail] = useState(''); // estado da sessão
-
-  async function handleSubmit(event) {
-    event.preventDefault();
-
-    const response = await api.post('/sessions', {
-      email
-    });
-
-    const { _id } = response.data;
-
-    localStorage.setItem('user', _id); //um storage do próprio navegador.
-//este localStorage pode ser visto pela ferramenta do desenvolvedor do navegador, em application, na parte de Storage, Local Storage, página da aplicação.
-
-    //console.log(_id);
-  }
-
+  
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
 
       <div className="content">
-        <p>
-          Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
-        </p>
-
-        <form onSubmit={handleSubmit}>
-          <label htmlFor="email">E-MAIL *</label>
-          <input 
-            type="email"
-            id="email" 
-            placeholder="Seu melhor e-mail"
-            onChange={event => setEmail(event.target.value)} // arrowFunction setEmail
-          />
-          <button type="submit" className="btn">Entrar</button>
-
-        </form>
+        <Routes />
+        
       </div>
     </div>
   );
---
+++ frontend/src/pages/Login/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import api from '../../services/api';
+
+export default function Login(){
+    const [email, setEmail] = useState(''); // estado da sessão
+
+    async function handleSubmit(event) {
+      event.preventDefault();
+  
+      const response = await api.post('/sessions', {
+        email
+      });
+  
+      const { _id } = response.data;
+  
+      localStorage.setItem('user', _id); //um storage do próprio navegador.
+  //este localStorage pode ser visto pela ferramenta do desenvolvedor do navegador, em application, na parte de Storage, Local Storage, página da aplicação.
+  
+      //console.log(_id);
+    }
+
+    return (
+        <>
+            <p>
+            Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
+            </p>
+
+            <form onSubmit={handleSubmit}>
+            <label htmlFor="email">E-MAIL *</label>
+            <input 
+                type="email"
+                id="email" 
+                placeholder="Seu melhor e-mail"
+                onChange={event => setEmail(event.target.value)} // arrowFunction setEmail
+            />
+            <button type="submit" className="btn">Entrar</button>
+
+            </form>
+        </>
+    )
+}
