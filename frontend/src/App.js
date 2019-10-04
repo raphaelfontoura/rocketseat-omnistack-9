b195a3af262bdff69cad6af7aa7f818856dbac90
Raphael Fontoura <raphael.fontoura@gmail.com>
2019-10-04 16:20:56 -0300

    configurado o axios com o arquivo api.js. Criado a captura do input de email e criado uma constante para manter a sessao do email. Porém será necessário ajustar o servidor por conta do erro de CORS (Cross-Origin Response) do servidor backend.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import api from './services/api';
 import './App.css';
 
 import logo from './assets/logo.svg';
 
 function App() {
+  const [email, setEmail] = useState(''); // estado da sessão
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    const response = await api.post('/sessions', {
+      email
+    });
+    console.log(response);
+  }
+
   return (
     <div className="container">
       <img src={logo} alt="AirCnC"/>
@@ -13,12 +25,13 @@ function App() {
           Ofereça <strong>spots</strong> para programadores e encontre <strong>talentos</strong> para sua empresa.
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit}>
           <label htmlFor="email">E-MAIL *</label>
           <input 
             type="email"
             id="email" 
             placeholder="Seu melhor e-mail"
+            onChange={event => setEmail(event.target.value)} // arrowFunction setEmail
           />
           <button type="submit" className="btn">Entrar</button>
 
